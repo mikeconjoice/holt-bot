7beb984fadf4091ec06acb66e6a14b067be0c6b1
Mike Conjoice <mikeconjoice@gmail.com>
2021-07-29 21:59:33 +0100

    .

--- holt.js
+++ holt.js
@@ -26,38 +26,38 @@ const emoji = ["🔥🔥🔥", "🔥🎺🔥", "🔥🙋‍♀️🔥"];
 // Here a tweet event is triggered!
 function tweetEvent(tweet, mediaId) {
 
-    var id = tweet.id_str;
-    var text = tweet.text;
-    var name = tweet.user.screen_name;
+  var id = tweet.id_str;
+  var text = tweet.text;
+  var name = tweet.user.screen_name;
 
-    //from itsAydrian in twitch chat on 1/28 😘    
-    let i = Math.floor(Math.random() * 3);
+  //from itsAydrian in twitch chat on 1/28 😘    
+  let i = Math.floor(Math.random() * 3);
 
-    // checks text of tweet for mention of Shania Bot
-    if ((text.includes('@positiveholt'))) {
+  // checks text of tweet for mention of Shania Bot
+  if ((text.includes('@positiveholt'))) {
 
-      // Start a reply back to the sender
-      var replyText = emoji[i] + "@" + name + " YASSSSS!!! ";
-      var b64content = fs.readFileSync('yas.gif', { encoding: 'base64' })
+    // Start a reply back to the sender
+    var replyText = emoji[i] + "@" + name + " YASSSSS!!! ";
+    var b64content = fs.readFileSync('yas.gif', { encoding: 'base64' })
 
-      // first we must post the media to Twitter
-      T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-        // now we can assign alt text to the media, for use by screen readers and
-        // other text-based presentations and interpreters
-        var mediaIdStr = data.media_id_string
-        var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+    // first we must post the media to Twitter
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdStr = data.media_id_string
+      var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-          if (!err) {
-            var params = { status: replyText, media_ids: [mediaIdStr] }
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          var params = { status: replyText, media_ids: [mediaIdStr] }
 
-            // Post that tweet
-            T.post('statuses/update', params)
-          }
+          // Post that tweet
+          T.post('statuses/update', params)
         }
-               )}
-        )}
+      }
+      )
     }
+    )
   }
 }
